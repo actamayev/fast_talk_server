d3e4e238f4fe031a901f3b9cc8f2343e747ed15f
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-12 17:53:56 -0400

    Proper module exports

--- src/handlers/auth_handlers/login.rs
+++ src/handlers/auth_handlers/login.rs
@@ -1,24 +1,24 @@
-//src/handlers/auth_handlers
+//src/handlers/login
 use actix_web::{web, HttpResponse, Error};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct RegisterRequest {
+pub struct LoginRequest {
     pub username: String,
     pub password: String,
 }
 
 #[derive(Serialize)]
-pub struct RegisterResponse {
+pub struct LoginResponse {
     pub access_token: String,
     pub public_key: String,
 }
 
-pub async fn register(req: web::Json<RegisterRequest>) -> Result<HttpResponse, Error> {
+pub async fn login(req: web::Json<LoginRequest>) -> Result<HttpResponse, Error> {
     let access_token = "some_generated_jwt".to_string();
     let public_key = "some_public_key".to_string();
 
-    let response = RegisterResponse {
+    let response = LoginResponse {
         access_token,
         public_key,
     };
