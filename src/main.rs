593abb0d026c6fa29853a9369ac7ddc9329e322e
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-12 22:11:35 -0400

    Add health endpoint

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ mod routes;
 mod middleware;
 mod handlers;
 
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web};
+use handlers::health_handler::health_check;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .configure(routes::auth_routes::auth_routes) // Configure all auth routes
+            .route("/health", web::get().to(health_check)) // Add the /health route directly
+
     })
     .bind("127.0.0.1:8080")?
     .run()
